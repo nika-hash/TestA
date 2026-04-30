2dfef80452380ab1dcc2752664f1239a8a7a44c5
unknown <nanokereselidze13u@gmaile.com>
2026-04-30 18:23:28 +0400

    update layout

--- Frontend/FinalProject/src/App.jsx
+++ Frontend/FinalProject/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Header from './layout/header'
+import Main from './layout/main'
+import Footer from './layout/footer'
 
 function App() {
 
   return (
     <>
-    
+    <Header/>
+    <Main>
+
+    </Main>
+    <Footer/>
     </>
   )
 }
---
+++ Frontend/FinalProject/src/layout/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function main(props) {
+  return (
+    <div>{props.children}</div>
+  )
+}
+
+export default main
